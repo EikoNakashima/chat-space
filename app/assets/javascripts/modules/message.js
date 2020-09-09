72c2cc8a0aea0639e2804b1c9d07c8dfe9835ff1
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-09-09 19:28:09 +0900

    commit

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -55,7 +55,6 @@ $(function(){
     .done(function(data){
       let html = buildHTML(data);
       $('.MessageField').append(html);
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});    
       $('form')[0].reset();
     })
     .always(() => {
